061c90623ac7e8bc0e23e8d705454cfb14bbd5cc
Sacha Bouillez <sachabouillez@gmail.com>
2020-02-17 12:59:36 +0100

    set up jest

--- src/reddit/ZoeMainsSubredditFetcher.ts
+++
@@ -1,8 +0,0 @@
-import { SubredditFetcher } from './SubredditFetcher';
-
-//* Implementation of SubredditFetcher for r/zoemains *//
-export class ZoeMainsSubredditFetcher extends SubredditFetcher {
-  constructor() {
-    super('aww');
-  }
-}

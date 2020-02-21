44da984c3c501d87b8c8844678ac6dcce841b6b1
Sacha Bouillez <sachabouillez@gmail.com>
2020-02-21 13:45:09 +0100

    implement twitter posting

--- src/twitter/TwitterPoster.ts
+++ src/twitter/TwitterPoster.ts
@@ -1,9 +1,42 @@
-import twitter from 'twitter';
+import Twitter, { AccessTokenOptions } from 'twitter';
+import { RedditPost } from '../reddit/RedditPost';
 
-const config = {
-  consumer_key: process.env.T_CONSUMER_KEY,
-  consumer_secret: process.env.T_CONSUMER_SECRET,
-  access_token_key: process.env.T_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.T_ACCESS_TOKEN_SECRET,
+export class TwitterPoster {
+  private T: Twitter;
+
+  constructor() {
+    if (
+      !process.env.T_CONSUMER_KEY ||
+      !process.env.T_CONSUMER_SECRET ||
+      !process.env.T_ACCESS_TOKEN_KEY ||
+      !process.env.T_ACCESS_TOKEN_SECRET
+    ) {
+      throw new Error('Missing Twitter config in env');
+    }
+    this.T = new Twitter({
+      consumer_key: process.env.T_CONSUMER_KEY,
+      consumer_secret: process.env.T_CONSUMER_SECRET,
+      access_token_key: process.env.T_ACCESS_TOKEN_KEY,
+      access_token_secret: process.env.T_ACCESS_TOKEN_SECRET,
+    });
+  }
+
+  postReddit =  async(post: RedditPost): Promise<void> => {
+    const twitterPost = new TwitterPost(post);
+
+    this.T.post('statuses/update', { status: twitterPost.status });
+  };
+
+  postTweet = async (tweet: string): Promise<void> => {
+    await this.T.post('statuses/update', { status: tweet });
+  };
+
+  
+}
+export class TwitterPost {
+  status: string;
+
+  constructor(post: RedditPost) {
+    this.status = `${post.title} by u/${post.author} ${post.url}`;
+  }
 };
-

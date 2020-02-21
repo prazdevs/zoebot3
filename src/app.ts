6efc027b3171251d8736426ce905d96bcd8a7bfa
PraZ <sachabouillez@gmail.com>
2020-02-22 00:25:27 +0100

    Fix an oopsie

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,7 @@
 import { DiscordBot } from './DiscordBot';
-import { SubredditFetcher } from './reddit/SubredditFetcher';
 
 require('dotenv').config();
 
 const zoeBot = new DiscordBot();
 
-//zoeBot.connect();
-
-const fetcher = new SubredditFetcher('zoemains');
-fetcher.getCompleteLatestPosts().then(submissions => {
-  submissions.forEach(submission => {
-    console.log(submission.link_flair_text);
-    console.log(submission.link_flair_type);
-  });
-});
+zoeBot.connect();
